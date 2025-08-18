495dd7dc4127f8707cb85ad6cd9b80e456851c3d
lucas-mansson <lu5132ma-s@student.lu.se>
2025-08-18 22:18:48 +0200

    read file

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
-package httpfromtcp
+package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Print("Hello")
+	
+	file, err := os.Open("messages.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		data := make([]byte, 8) // Allocate and initialize empty array of bytes
+		nbrBytes, err := file.Read(data) // reads 8 bytes from file and stores in data
+		
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("read: %s\n", string(data[:nbrBytes])) // returns and removes the first n bytes from the array
+	}
+
 }
