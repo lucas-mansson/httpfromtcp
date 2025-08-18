8ce178b49d2edf1dce366b115359ea5868e1c11a
lucas-mansson <lu5132ma-s@student.lu.se>
2025-08-18 23:02:01 +0200

    create channel for reading lines

--- main.go
+++ main.go
@@ -6,26 +6,44 @@ import (
 	"os"
 )
 
-func main() {
-	
-	file, err := os.Open("messages.txt")
+func getLinesChannel(file io.ReadCloser) <- chan string {
+	out := make(chan string, 1)
 
+	go func() {
+		defer file.Close()
+		defer close(out)
+
+		var currentLine string
+		for {
+			data := make([]byte, 8) // Allocate and initialize empty array of 8 bytes
+			nbrBytes, err := file.Read(data) // reads 8 bytes from file and stores in data
+
+			if err == io.EOF { break }
+			if err != nil { panic(err) }
+
+			currentSlice := data[:nbrBytes]
+
+			for _, byte := range currentSlice {
+				if byte == '\n' {
+					out <- string(currentLine)
+					currentLine = ""
+				} else {
+					currentLine += string(byte)
+				}
+			}
+		}
+	}()
+	return out
+}
+
+func main() {
+	file, err := os.Open("messages.txt")
 	if err != nil {
 		panic(err)
 	}
-
-	for {
-		data := make([]byte, 8) // Allocate and initialize empty array of bytes
-		nbrBytes, err := file.Read(data) // reads 8 bytes from file and stores in data
-		
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Printf("read: %s\n", string(data[:nbrBytes])) // returns and removes the first n bytes from the array
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read %s\n", line)
 	}
-
 }
+
